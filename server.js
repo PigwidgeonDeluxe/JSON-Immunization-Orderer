ded1b944c74a6332e70da5a8a1bfb54b12f0dc30
PigwidgeonDeluxe <jeff.jaguar@live.ca>
2016-10-11 16:36:10 -0400

    server.js
    Changed sort order: sorts by name, then by ID for any repeats.

--- server.js
+++ server.js
@@ -12,7 +12,23 @@ app.use(bodyParser.urlencoded({
 //function that dictates how to sort the id
 function sortID(a, b) {
         //sort the ids by ascending order
+        //console.log(parseFloat(a.resource.id), parseFloat(b.resource.id))
         return  parseFloat(a.resource.id) -  parseFloat(b.resource.id);
+    }
+
+//function that dictates how to sort the names
+function sortName(a, b) {
+        //sort the names by ascending order
+        var name_a = (a.resource.vaccineCode.coding[0].display);
+        var name_b = (b.resource.vaccineCode.coding[0].display);
+
+        //if the two immunizations are the same, sort by id number
+        if (name_a == name_b){
+            return sortID(a, b);
+        } else {
+            return (name_a).localeCompare(name_b);
+        }
+
     }
 
 //receive GET request from anywhere
@@ -23,7 +39,6 @@ app.get('/', function(req, res) {
 
 app.post('/process_post', function(req, res) {
     // catch any errors caused by improper input JSON formatting
-    try {
         //get the unformatted json submitted
         response = {
             body_json: req.body.body_json,
@@ -32,17 +47,34 @@ app.post('/process_post', function(req, res) {
         var rawJSON = JSON.parse(response["body_json"]);
         //get entry key
         var unformattedEntry = rawJSON.entry;
-        //sort the unformatted JSON's immunization
-        unformattedEntry.sort(sortID);
+    //console.log(unformattedEntry);
+        //list of keys to not be sorted
+        var noSort = [];
+        //temporarily remove all keys that will not be sorted
+        for (var k in unformattedEntry){
+            if (unformattedEntry[k].resource.resourceType != "Immunization"){
+                noSort.push(unformattedEntry[k]);
+                delete unformattedEntry[k];
+                //console.log("noSort", noSort);
+            }
+        }
+
+        //sort the sorted ids by name
+        var formattedName = unformattedEntry.sort(sortName);
+
+        //put the temporarily removed keys back into the now sorted array
+        for (var k in noSort){
+            formattedName.push(noSort[k]);
+        }
+
+        //remove all null values from the array
+        var resultSort = formattedName.filter(Boolean);
         
         //put the now ordered immunizations back and send the formatted json back
-        rawJSON.entry = unformattedEntry;
+        rawJSON.entry = resultSort;
 
         res.end(JSON.stringify(rawJSON, null, 4));
-    }
-    catch(err) {
-     res.end("An Error occurred. Please check the formatting of the JSON input and ensure that it has the form entry.resource.id.");
-    }
+        
 
 })
 
